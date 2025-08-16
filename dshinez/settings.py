992d899463d855ae1bbf40767a643d62eb1d8610
Rohit N <rohitnnair24@gmail.com>
2025-08-16 15:32:27 +0530

    Add EC2 IP to ALLOWED_HOSTS and update settings

--- dshinez/settings.py
+++ dshinez/settings.py
@@ -27,10 +27,9 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["51.20.114.89", "localhost", "127.0.0.1"]
 
 
 # Application definition
