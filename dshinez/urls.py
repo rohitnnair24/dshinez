d2f0ccd6de9a4dc05a838cca8cf528e4242ae4fc
Rohit N <rohitnnair24@gmail.com>
2025-08-18 12:18:13 +0530

    updated settings.py and urls.py

--- dshinez/urls.py
+++ dshinez/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('bookquotes.urls')),
+    path('bookquotes/', include('bookquotes.urls')),  # ✅ add prefix
 ]
+
 
