abae86a182be54cd2fa1859f3b57f216f1dbe299
Rohit N <rohitnnair24@gmail.com>
2025-08-18 16:05:34 +0530

    Settings.py is updated

--- dshinez/settings.py
+++ dshinez/settings.py
@@ -97,7 +97,7 @@ SIMPLE_JWT = {
     'AUTH_COOKIE_SECURE': False,   
     'AUTH_COOKIE_HTTP_ONLY': True,
     'AUTH_COOKIE_PATH': '/',
-    'AUTH_COOKIE_SAMESITE': 'None',  
+    'AUTH_COOKIE_SAMESITE': 'Lax',
 }
 
 
