4197fe575211a695e31b7f67a11fae905733221d
Rohit N <rohitnnair24@gmail.com>
2025-08-19 11:53:11 +0530

    Allow any on views get contact

--- bookquotes/views.py
+++ bookquotes/views.py
@@ -172,7 +172,7 @@ def submit_contact(request):
 
 # ✅ Get all contacts (Admin only)
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])  
+@permission_classes([AllowAny])  
 def get_all_contacts(request):
     contacts = ContactRequest.objects.all().order_by('-submitted_at')
     serializer = ContactRequestSerializer(contacts, many=True)
